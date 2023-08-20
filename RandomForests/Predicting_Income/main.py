52cb7bc61c0277cd120f4a807a33efd1a3f7808e
gaiborjosue <gaiborjimenezjosue@gmail.com>
2023-08-20 15:28:27 -0500

    feat(RF): Completed Main.py model

---
+++ RandomForests/Predicting_Income/main.py
@@ -0,0 +1,27 @@
+def warn(*args, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+
+income_data = pd.read_csv("income.csv", header = 0, delimiter = ", ")
+
+income_data["sex-int"] = income_data["sex"].apply(lambda row: 0 if row == "Male" else 1)
+
+
+labels = income_data[["income"]]
+
+data = income_data[["age", "capital-gain", "capital-loss", "hours-per-week", "sex-int"]]
+
+train_data, test_data, train_labels, test_labels = train_test_split(data, labels, random_state=1)
+
+forest = RandomForestClassifier(random_state = 1)
+
+forest.fit(train_data, train_labels)
+
+print(forest.feature_importances_)
+print(forest.score(test_data, test_labels))

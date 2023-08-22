fe5dceeada3763583cc92e50bdc97241bcdbdf61
gaiborjosue <gaiborjimenezjosue@gmail.com>
2023-08-22 11:36:22 -0500

    t

--- .github/workflows/getPythonScripts.py
+++ .github/workflows/getPythonScripts.py
@@ -1,5 +1,6 @@
 import os
 import re
+import subprocess
 
 # Get the python_scripts environment variable
 python_scripts = os.environ.get("pythons")
@@ -7,14 +8,11 @@ python_scripts = os.environ.get("pythons")
 # Split the URLs into a list
 urls = python_scripts.strip().split('\n')
 
-# Initialize a list to hold the generated SVN URLs
-svn_urls = []
 
 # Loop through each URL
 for url in urls:
     # Replace /tree/branchName or /blob/branchName with /trunk
     svn_url = re.sub(r'/tree/[^/]+|/blob/[^/]+', '/trunk', url)
-    svn_urls.append(svn_url)
-
-# Print the generated SVN URLs separated by newline
-print('\n'.join(svn_urls))
+    
+    # Perform the svn export using subprocess
+    subprocess.run(["svn", "export", svn_url, "./${{steps.get_path.outputs.path}}"])

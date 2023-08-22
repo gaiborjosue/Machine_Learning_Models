dbc55d944468367a99baffb96c4f87b83bf47cd0
gaiborjosue <gaiborjimenezjosue@gmail.com>
2023-08-22 10:45:00 -0500

    test

---
+++ .github/workflows/getPythonScripts.py
@@ -0,0 +1,20 @@
+import os
+import re
+
+# Get the python_scripts environment variable
+python_scripts = os.environ.get("pythons")
+
+# Split the URLs into a list
+urls = python_scripts.split('\n')
+
+# Initialize a list to hold the generated SVN URLs
+svn_urls = []
+
+# Loop through each URL
+for url in urls:
+    # Replace /tree/branchName or /blob/branchName with /trunk
+    svn_url = re.sub(r'/tree/[^/]+|/blob/[^/]+', '/trunk', url)
+    svn_urls.append(svn_url)
+
+# Print the generated SVN URLs separated by newline
+print('\n'.join(svn_urls))
